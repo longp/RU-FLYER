735e31828c4cea9ec30c68fee518b47dde9993f4
long phan <longphan@nbp-146-55.nbp.ruw.rutgers.edu>
2016-02-25 20:43:54 -0500

    added validition to user model

--- server.js
+++ server.js
@@ -4,17 +4,18 @@ var Sequelize = require("sequelize");
 var bodyParser = require("body-parser");
 var session = require('express-session');
 var PORT = process.env.PORT || 3000;
-
+var app =express();
+//handlebars setup
 app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
 app.set("view engine", 'handlebars');
 
-
+//static routes for js and css
 app.use("/js", express.static("public/js"));
-app.use("/css", express.static("public/css"));s
+app.use("/css", express.static("public/css"));
 
-
+//routes for handlebars render
 app.get("/", function (req, res) {
   res.render("home");
 });
@@ -25,26 +26,65 @@ app.get("/login", function (req, res) {
 
 
 //database setup
-var Sequelize = require('sequelize');
 var connection = new Sequelize('users', 'root', '', {
   dialect: 'mysql',
   port: 3306,
   host: 'localhost'
 });
 
-var user = connection.define('user', {
-  username: Sequelize.STRING,
-  password: Sequelize.STRING,
-  firstName: Sequelize.STRING,
-  lastName: Sequelize.STRING,
-  email: Sequelize.STRING
-}), {
+
+var User = connection.define('user', {
+  username: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate : {
+      notEmpty:true,
+      isAlphanumeric: true
+      }
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate : {
+        notEmpty: true,
+        len: {
+          args:[8, 20],
+          msg: "Password must be 8-20 characters long"
+        }
+      }
+    },
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate : {
+        notEmpty: true,
+        is: ["^[a-z]+$", 'i']
+      }
+    },
+    lastName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate : {
+        notEmpty: true,
+        is: ["^[a-z]+$", 'i']
+      }
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate : {
+        isEmail: true
+      }
+    }
+},
+  {
   hooks: {
     beforeCreate :function (argument) {
 
     }
   }
-}
+});
 
 // database connection via sequelize
 connection.sync().then(function() {
