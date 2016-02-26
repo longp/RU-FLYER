daf7530af39a21a9109f922bf95fcd969c3dd09b
Alexg2195 <alexg2195@gmail.com>
2016-02-25 21:18:06 -0500

    working form input with salted password

--- server.js
+++ server.js
@@ -3,18 +3,28 @@ var exphbs = require("express-handlebars");
 var Sequelize = require("sequelize");
 var bodyParser = require("body-parser");
 var session = require('express-session');
+var bcrypt = require("bcryptjs");
+var passport = require('passport');
+var passportLocal = require('passport-local');
 const PORT = process.env.PORT || 3000;
+var app = express();
 
+// body parser setup
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+//handlebars setup
 app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
 app.set("view engine", 'handlebars');
 
-
+// simplify routing
 app.use("/js", express.static("public/js"));
-app.use("/css", express.static("public/css"));s
+app.use("/css", express.static("public/css"));
 
-
+// routes
 app.get("/", function (req, res) {
   res.render("home");
 });
@@ -23,6 +33,16 @@ app.get("/login", function (req, res) {
   res.render("login");
 });
 
+app.post('/register', function (req, res) {
+  console.log(req.body);
+  user.create({
+    username: req.body.username,
+    password: req.body.password,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email
+  });
+});
 
 //database setup
 var Sequelize = require('sequelize');
@@ -32,19 +52,26 @@ var connection = new Sequelize('users', 'root', '', {
   host: 'localhost'
 });
 
-var user = connection.define('user', {
-  username: Sequelize.STRING,
-  password: Sequelize.STRING,
+var user = connection.define('users', {
+  username: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
   firstName: Sequelize.STRING,
   lastName: Sequelize.STRING,
   email: Sequelize.STRING
-}), {
+}, {
   hooks: {
-    beforeCreate :function (argument) {
-
+    beforeCreate: function(input){
+      input.password = bcrypt.hashSync(input.password, 10);
     }
   }
-}
+});
 
 // database connection via sequelize
 connection.sync().then(function() {
