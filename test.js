8833a3226c73f08a0f016edee52f90bc2620142f
Alexg2195 <alexg2195@gmail.com>
2016-03-05 00:00:44 -0500

    well heroku db is limiting my connections to it sooooo

--- test.js
+++ test.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 var Sequelize = require("sequelize");
 var connection = new Sequelize(process.env.JAWSDB_URL);
+
 var Event = connection.define('event', {
   event: {
     type: Sequelize.STRING,
@@ -46,6 +47,79 @@ var Event = connection.define('event', {
     }
   }
 });
+
+var Attending = connection.define('attendance', {
+  eventId: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate : {
+      notEmpty:true,
+    }
+  },
+  user: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate : {
+      notEmpty: true
+    }
+  }
+});
+
+var User = connection.define('user', {
+  username: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate : {
+      notEmpty:true,
+      isAlphanumeric: true
+    }
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate : {
+      notEmpty: true,
+      len: {
+        args:[8, 20],
+        msg: "Password must be 8-20 characters long"
+      }
+    }
+  },
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate : {
+      notEmpty: true,
+      is: ["^[a-z]+$", 'i']
+    }
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate : {
+      notEmpty: true,
+      is: ["^[a-z]+$", 'i']
+    }
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate : {
+      isEmail: true
+    }
+  }
+},
+  {
+  hooks: {
+    beforeCreate: function(input){
+      input.password = bcrypt.hashSync(input.password, 10);
+    }
+  }
+});
+
+User.sync();
+Attending.sync();
 Event.sync();
 
 // ------------^^^^setting for testing only^^^^----------------
@@ -63,19 +137,49 @@ var transporter = nodemailer.createTransport('smtps://' + sender.email + '%40' +
 // });
 
 function runEventChecks () {
-  var event;
   var recipients = [];
+  var recipientsEmails = [];
+  var date = new Date();
+  var year = date.getFullYear();
+  var month = date.getMonth();
+  var day = date.getDate();
+  var fullDate = year + "-" + padDate(month + 1) + "-" + padDate(day + 1);
+  //This^^^^^is really tomorrows date to test for a event coming up
+  console.log(fullDate);
 
   Event.findAll({
-
-  }).then(function(results) {
-    console.log(results);
-    console.log(results.dataValues);
+  }).then(function(allEvents) {
+    for (var i = allEvents.length - 1; i >= 0; i--) {
+      console.log(allEvents[i].date);
+      console.log(allEvents[i].id);
+      if (allEvents[i].date === fullDate) {
+        Attending.findAll({
+          where: {eventId: allEvents[i].id}
+        }).then(function (attendingUsers) {
+          console.log("attending users:" + attendingUsers);
+          for (var i = attendingUsers.length - 1; i >= 0; i--) {
+            recipients.push(attendingUsers[i].username);
+          }
+          console.log("resipients: " + recipients);
+          User.findAll({
+            where: {username: recipients}
+          }).then(function (results) {
+            console.log(results);
+            // console.log(results[0].email);
+          })
+        })
+      }
+    }
   })
 }
 
 runEventChecks();
 
+function padDate (num) {
+  if (num < 10) {
+    return ("0" + num);
+  }
+}
 
 function sendEmail (receivers, eventInfo) {
 
